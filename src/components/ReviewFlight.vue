5b311471546e82fb3bcdde4f1d2af4fa30070ec6
Ben Dunn <benthedunn@gmail.com>
2020-09-28 16:49:58 -0600

    ticket formatting

--- src/components/ReviewFlight.vue
+++ src/components/ReviewFlight.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
+  <div class="ticket">
     <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
-    <div class="ticket">
-      <p>
-      {{flightChoice}}
-      </p>
-    </div>
+    <ul class="labels">
+      <li>Departure:</li>
+      <li>From:</li>
+      <li>Arrival:</li>
+      <li>To:</li>
+      <li>Type:</li>
+      <li>Flight #:</li>
+      <li>Cost:</li>
+    </ul>
+    <p>
+    {{flightChoice}}
+    </p>
   </div>
 </template>
 
@@ -24,11 +31,23 @@ export default {
   p{
     font-family: 'Source Code Pro', monospace;  
     font-size: 18pt;
+    color: wheat;
+    text-align: right;
+    align-self: flex-end;
+  }
+
+  ul{
+    list-style: none;
+    font-family: 'Source Code Pro', monospace;  
+    font-size: 18pt;
+    color: wheat;
+    text-align: left;
   }
 
   .ticket{
     display: flex;
     justify-content: center;
-    width: 45%;
+    align-content: space-between;
+    width: 100%;
   }
 </style>
